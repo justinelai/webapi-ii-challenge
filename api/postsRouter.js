7f2ac336ea5b629aa642ea7e4eecc36fc5f8bc5e
justinelai <justine.lai@gmail.com>
2019-09-03 15:09:20 -0400

    PUT to api/posts/:id working

--- api/postsRouter.js
+++ api/postsRouter.js
@@ -53,5 +53,26 @@ router.get('/posts/:id', (req, res) => {
     })
 })
 
+// ========================= PUT /api/posts/:id =========================
+
+router.put('/posts/:id', (req, res) => {
+    const id = req.params.id
+    const {title, contents} = req.body;
+    if (!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        Posts.update(id, req.body)
+    .then(post => {
+        post === 1 ?
+        res.status(201).json(req.body) :
+        res.status(404).json({ message: "The post with the specified ID does not exist." }) 
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The post information could not be modified." })
+    })
+    }
+
+})
+
 
 module.exports = router;
