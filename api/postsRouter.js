fc098b3a418fce6463704be31a5606b482e7f96e
justinelai <justine.lai@gmail.com>
2019-09-03 14:58:03 -0400

    GET to api/posts/:id working

--- api/postsRouter.js
+++ api/postsRouter.js
@@ -39,7 +39,19 @@ router.get('/posts', (req, res) => {
     }
 );
 
+// ========================= GET /api/posts/:id =========================
 
+router.get('/posts/:id', (req, res) => {
+    const id = req.params.id
+    Posts.findById(id)
+    .then(post => {
+        post == 0 ? res.status(404).json({ message: "The post with the specified ID does not exist." })
+        : res.status(200).json(post) 
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    })
+})
 
 
 module.exports = router;
